5db271cc7e1fd911f8fb2c6372ba40c27b1c974a
angelolisa26 <55422054+angelolisa26@users.noreply.github.com>
2022-09-27 02:28:37 +0200

    nre

---
+++ config/initializers/blockchain_api.rb
@@ -0,0 +1,8 @@
+Peatio::Blockchain.registry[:bitcoin] = Bitcoin::Blockchain
+Peatio::Blockchain.registry[:geth] = Ethereum::Eth::Blockchain
+Peatio::Blockchain.registry[:parity] = Ethereum::Eth::Blockchain
+Peatio::Blockchain.registry[:"geth-bsc"] = Ethereum::Bsc::Blockchain
+Peatio::Blockchain.registry[:"geth-heco"] = Ethereum::Heco::Blockchain
+Peatio::Blockchain.registry[:fiat] = Peatio::Fiat
+Peatio::Blockchain.registry[:sato] = Sato::Blockchain
+Peatio::Blockchain.registry[:kawkaw] = kawkaw::Blockchain
---
+++ config/initializers/wallet_api.rb
@@ -0,0 +1,10 @@
+Peatio::Wallet.registry[:bitcoind] = Bitcoin::Wallet
+Peatio::Wallet.registry[:geth] = Ethereum::Eth::Wallet
+Peatio::Wallet.registry[:parity] = Ethereum::Eth::Wallet
+Peatio::Wallet.registry[:gnosis] = Gnosis::Wallet
+Peatio::Wallet.registry[:"ow-hdwallet-eth"] = OWHDWallet::WalletETH
+Peatio::Wallet.registry[:"ow-hdwallet-bsc"] = OWHDWallet::WalletBSC
+Peatio::Wallet.registry[:"ow-hdwallet-heco"] = OWHDWallet::WalletHECO
+Peatio::Wallet.registry[:opendax_cloud] = OpendaxCloud::Wallet
+Peatio::Wallet.registry[:satod] = sato::Wallet
+Peatio::Wallet.registry[:kawkawd] = kawkaw::Wallet
